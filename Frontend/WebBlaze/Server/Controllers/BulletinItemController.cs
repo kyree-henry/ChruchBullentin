f77120b7502d75eb1dc50cde32f52b3a6efc755a
Kyree henry <ene.henry.eh@gmail.com>
2023-11-10 13:10:57 +0100

    #10 Added User Interface Project ( Blazor WASM )

---
+++ Frontend/WebBlaze/Server/Controllers/BulletinItemController.cs
@@ -0,0 +1,25 @@
+﻿using ChruchBulletin.Core.Entity;
+using ChruchBulletin.Core.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChruchBulletin.WebBlaze.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BulletinItemController : ControllerBase
+    {
+        private readonly IBulletinItemByDateHandler _handler;
+
+        public BulletinItemController(IBulletinItemByDateHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpGet]
+        public IEnumerable<BulletinItem> Get()
+        {
+            IEnumerable<BulletinItem> items = _handler.Handle(new BulletinItemByDateAndTimeQuery(new DateTime(2000, 1, 1)));
+            return items;
+        }
+    }
+}
